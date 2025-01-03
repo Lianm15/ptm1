fc09c8076c81998dade9ec3f9048ef26030a4b3d
DESKTOP-JJTV465\lianm <lianmh1500@gmail.com>
2025-01-03 22:04:20 +0200

    TopicManagerSingleton class implementation

--- test/TopicManagerSingleton.java
+++ test/TopicManagerSingleton.java
@@ -1,9 +1,48 @@
 package test;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TopicManagerSingleton {
 
+
     public static class TopicManager{
+
+        private static final TopicManager instance = new TopicManager();
+        Map<String,Topic> topics;
+
+        private TopicManager() {
+            this.topics = new HashMap<>();
+
+        }
+
+        public synchronized Topic getTopic(String name)
+        {
+            if(!topics.containsKey(name))
+            {
+                topics.put(name, new Topic(name));
+
+            }
+            return topics.get(name);
+        }
+
+        public Collection<Topic> getTopics()
+        {
+            return topics.values();
+
+        }
+
+        public void clear()
+        {
+            topics.clear();
+
+        }
+
+    }
+
+    public static TopicManager get() {
+        return TopicManager.instance;
     }
 
     
